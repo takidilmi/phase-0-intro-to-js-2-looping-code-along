65055b72074309e332c4bb57921707b6eb0b829e
takidilmi <angeldilmi@gmail.com>
2023-07-22 12:57:11 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names, eventnames) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++){
+        const message = `Thank you, ${names[i]}, for the wonderful ${eventnames} gift!`;
+        thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+}
+
+
+let names = ["Guadalupe", "Ollie", "Aki"];
+let event = "surprise";
+const thankYouMessages = writeCards(names, event);
+console.log(thankYouMessages);
+
+
+function countDown(x) {    
+    while (x>=0) {
+        console.log(x);
+        x--;
+    }
+}
